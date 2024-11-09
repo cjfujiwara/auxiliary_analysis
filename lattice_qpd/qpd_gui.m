619c08c041c50fef3f4b9c91d1f5e4b768999285
Cora <63414958+cjfujiwara@users.noreply.github.com>
2024-11-08 19:11:30 -0500

    Create qpd_gui.m

---
+++ lattice_qpd/qpd_gui.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = qpd_gui(inputArg1,inputArg2)
+%QPD_GUI Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
