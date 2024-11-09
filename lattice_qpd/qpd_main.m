efa440e71d478a820786f66b64e47068fc7d0546
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2024-11-08 19:01:58 -0500

    updates

--- lattice_qpd/qpd_main.m
+++ lattice_qpd/qpd_main.m
@@ -18,6 +18,17 @@ function [figs,output] = qpd_main(npt,opts)
 
 % Source Directory for files
 
+% Display this filename
+disp(repmat('-',1,60));disp(repmat('-',1,60));    
+disp(['Calling ' mfilename '.m']);
+disp(repmat('-',1,60));disp(repmat('-',1,60));    
+
+% Add all subdirectories for this m file
+curpath = fileparts(mfilename('fullpath'));
+addpath(curpath);addpath(genpath(curpath));
+
+a = fileparts(curpath);
+addpath(a);addpath(genpath(a));
 
 calib = qpd_calibrations;
 
